70c3bd19170f20f7d205c4d435e48e32d84693a7
Nmlycette <78880054+Nmlycette@users.noreply.github.com>
2022-11-18 23:08:10 -0500

    Added input validation, refined cash/credit/check classes, and payment class

---
+++ Onederus_giftshop/Onederus_giftshop/CardPayment.cs
@@ -0,0 +1,212 @@
+﻿using Microsoft.VisualBasic;
+using Onederus_giftshop;
+using System;
+using System.Data;
+using System.Globalization;
+using System.Xml.Linq;
+
+namespace Onederus_giftshop
+{
+    public static class CardPayment
+    {
+        const int validCardLength = 16;
+        const int validCvvLength = 3;
+        const int validMonthLength = 2;
+        const int validYearLength = 2;
+        public static long CardNumber { get; set; }
+        public static string CardExp { get; set; }
+        public static int Cvv { get; set; }
+
+        public static void CardPay(double totalDue) //calls all card validation methods
+        {
+            bool continueWithCard = Payment.NonCashTransaction(totalDue);
+            GetCardNumber();
+            GetCardExp();
+            GetCardCvv();
+        }
+
+        public static long GetCardNumber()
+        {
+            bool validCardNum = false;
+
+            while (validCardNum == false)
+            {
+                Console.WriteLine("\nEnter 16 digit credit card number:");
+                CardNumber = InputValidation.IsLong();
+                int cardNumLength = CardNumber.ToString().Length;
+
+                if (cardNumLength != validCardLength)
+                {
+                    validCardNum = false;
+                }
+                else validCardNum = true;
+            }
+            return CardNumber;
+        }
+
+        public static string GetCardExp()
+        {
+            bool monthCaptured = false;
+            bool yearCaptured = false;
+            int year = 00;
+            int month = 00;
+
+            while (monthCaptured == false)
+            {
+                Console.WriteLine("\nEnter expiration month (MM):");
+                month = InputValidation.IsMonth();
+
+                int monthLength = month.ToString().Length;
+
+                if (monthLength != validMonthLength)
+                {
+                    monthCaptured = false;
+                }
+                else monthCaptured = true;
+                break;
+            }
+
+            while (yearCaptured == false)
+            {
+                Console.WriteLine("\nEnter expiration year (YY):");
+                year = InputValidation.IsYear();
+
+                int yearLength = year.ToString().Length;
+
+                if (yearLength != validYearLength)
+                {
+                    yearCaptured = false;
+                }
+                else yearCaptured = true;
+                break;
+            }
+
+            string MM = month.ToString();
+            string YY = year.ToString();
+            CardExp = ($"{MM}/{YY}");
+
+            return CardExp;
+        }
+
+        public static int GetCardCvv()
+        {
+            bool validCvv = false;
+
+            while (validCvv == false)
+            {
+                Console.WriteLine("\nEnter 3 digit cvv:");
+                Cvv = InputValidation.IsInt();
+                int checkNumLength = Cvv.ToString().Length;
+
+                if (checkNumLength != validCvvLength)
+                {
+                    validCvv = false;
+                }
+                else validCvv = true;
+            }
+            return Cvv;
+        }
+    }
+}
+{
+    public static class CardPayment
+    {
+        const int validCardLength = 16;
+        const int validCvvLength = 3;
+        const int validMonthLength = 2;
+        const int validYearLength = 2;
+        public static long CardNumber { get; set; }
+        public static string CardExp { get; set; }
+        public static int Cvv { get; set; }
+
+        public static void CardPay(double totalDue) //calls all card validation methods
+        {
+            bool continueWithCard = Payment.NonCashTransaction(totalDue);
+            GetCardNumber();
+            GetCardExp();
+            GetCardCvv();
+        }
+
+        public static long GetCardNumber()
+        {
+            bool validCardNum = false;
+
+            while (validCardNum == false)
+            {
+                Console.WriteLine("\nEnter 16 digit credit card number:");
+                CardNumber = InputValidation.IsLong();
+                int cardNumLength = CardNumber.ToString().Length;
+
+                if (cardNumLength != validCardLength)
+                {
+                    validCardNum = false;
+                }
+                else validCardNum = true;
+            }
+            return CardNumber;
+        }
+
+        public static string GetCardExp()
+        {
+            bool monthCaptured = false;
+            bool yearCaptured = false;
+            int year = 00;
+            int month = 00;
+
+            while (monthCaptured == false)
+            {
+                Console.WriteLine("\nEnter expiration month (MM):");
+                month = InputValidation.IsMonth();
+
+                int monthLength = month.ToString().Length;
+
+                if (monthLength != validMonthLength)
+                {
+                    monthCaptured = false;
+                }
+                else monthCaptured = true;
+                break;
+            }
+
+            while (yearCaptured == false)
+            {
+                Console.WriteLine("\nEnter expiration year (YY):");
+                year = InputValidation.IsYear();
+
+                int yearLength = year.ToString().Length;
+
+                if (yearLength != validYearLength)
+                {
+                    yearCaptured = false;
+                }
+                else yearCaptured = true;
+                break;
+            }
+
+            string MM = month.ToString();
+            string YY = year.ToString();
+            CardExp = ($"{MM}/{YY}");
+
+            return CardExp;
+        }
+
+        public static int GetCardCvv()
+        {
+            bool validCvv = false;
+
+            while (validCvv == false)
+            {
+                Console.WriteLine("\nEnter 3 digit cvv:");
+                Cvv = InputValidation.IsInt();
+                int checkNumLength = Cvv.ToString().Length;
+
+                if (checkNumLength != validCvvLength)
+                {
+                    validCvv = false;
+                }
+                else validCvv = true;
+            }
+            return Cvv;
+        }
+    }
+}
--- Onederus_giftshop/Onederus_giftshop/Program.cs
+++ Onederus_giftshop/Onederus_giftshop/Program.cs
@@ -1,18 +1,30 @@
 using Onederus_giftshop;
 
-
 ///Testing Methods in Payment Class
 //show test subtotal
-double testsubTotal = 20.00;
-Console.WriteLine($"Subtotal: {testsubTotal}");
+
+
+Console.WriteLine($"Subtotal: {testSubTotal}");
 
 //show tax for testing
 Console.WriteLine($"Tax: {Payment.Tax}");
 
 //show total due for testing
-double testtotalDue = Payment.GetTotalCost(testsubTotal);
+double testtotalDue = Payment.GetTotalCost(testSubTotal);
 Console.WriteLine($"Grand Total: {testtotalDue}\n");
 
+//show mayment method (1 cash, 2 check, 3 card)
 int paymentMethod = Payment.SelectPaymentType(testtotalDue);
-Console.WriteLine($"Payment Method = {paymentMethod}");
-// if 1 - cash, if 2 - check, if 3 - card
+Console.WriteLine($"Payment Method = {paymentMethod}\n");
+
+//show cash parameters
+Console.WriteLine($"Cash Tendered: {CashPayment.CashTendered}");
+Console.WriteLine($"Change Due: {CashPayment.ChangeDue}\n");
+
+//show card parameters
+Console.WriteLine($"Card Number: {CardPayment.CardNumber}");
+Console.WriteLine($"Card CVV: {CardPayment.Cvv}");
+Console.WriteLine($"Card Exp: {CardPayment.CardExp}\n");
+
+//show check parameters
+Console.WriteLine($"Check Number:{CheckPayment.CheckNum}");
